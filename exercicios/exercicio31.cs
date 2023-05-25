c746cdf455795539dd3d5709c44b371cb8459ce0
Gustavo Henrique Butzke <114582936+GustavoButzke@users.noreply.github.com>
2023-05-24 21:26:41 -0300

    rev

--- exercicios/exercicio31.cs
+++ exercicios/exercicio31.cs
@@ -5,81 +5,47 @@ using System;
 
 namespace CSharp
 {
-    public class Exercicio31
+    public class exercicio31
     {
-      public static main(string[] args)
+      public static void ex31(string[] args)
       {
-        int num = Convert.ToInt32(Console.ReadLine());
+        int op;
         do{
             Console.WriteLine("\n \t Escolha um numero de 1 a 3 para selecionar o tipo de combustível: \n");
+            int numero = Convert.ToInt32(Console.ReadLine());
+    
+            Console.WriteLine("\n [1] Álcool");
+            Console.WriteLine("\n [2] Gasolina");
+            Console.WriteLine("\n [3] Diesel");
+            Console.WriteLine("\n [7] Encerrar \n");
+            op = Convert.ToInt32(Console.ReadLine());
 
-            switch (num)
-            {
-                case 1 : {
-                    Console.WriteLine("\n \t Álcool \n");
-                    break;
-                }
-                case 2 : {
-                    Console.WriteLine("\n \t Gasolina \n");
-                    break;
-                }
-                case 3 : {
-                    Console.WriteLine("\n \t Diesel \n");
-                    break;
-                }
-            }
+            	switch(op) {
+			case 1: {
+                Console.WriteLine("\n \t tste \n");
+				break;
+			};
+			case 2: {
+                exercicio02.calculadora();
+                break;      
+			};
+			case 3: {
+                exrecicio03.criminoso();
+				break;
+			};
+			case 4: {
+				Console.WriteLine("Finalizando");
+				break;
+			};
+			default: {
+				Console.WriteLine("\nOpção inválida");
+				break;
+			}
+		}
+	} while(op != 7);
+	Console.WriteLine("\n\n\n\n\n - Até mais \n\n\n\n\n");
         }
-        while(num != 3);
-	    Console.WriteLine("\n\n\n\n\n - Até mais \n\n\n\n\n");
-      }
+
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//            Console.WriteLine("\n [1] Álcool");
-//            Console.WriteLine("\n [2] Gasolina");
-//            Console.WriteLine("\n [3] Diesel");
-//            Console.WriteLine("\n [7] Encerrar \n");
-//            op = Convert.ToInt32(Console.ReadLine());
-//
-//            	switch(op) {
-//			case 1: {
-//                Console.WriteLine("\n \t tste \n");
-//				break;
-//			};
-//			case 2: {
-//                exercicio02.calculadora();
-//                break;      
-//			};
-//			case 3: {
-//                exrecicio03.criminoso();
-//				break;
-//			};
-//			case 4: {
-//				Console.WriteLine("Finalizando");
-//				break;
-//			};
-//			default: {
-//				Console.WriteLine("\nOpção inválida");
-//				break;
-//			}
-//		}
-//	} while(op != 7);
-//	Console.WriteLine("\n\n\n\n\n - Até mais \n\n\n\n\n");
-//        }
-//
-//
-//    }
-//}
